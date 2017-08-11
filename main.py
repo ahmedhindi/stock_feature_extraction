b6f7ba8b0ac253ac4ff56def95dab64fc6e4ab0c
ahmedhindi <ahmedelsyd5@gmail.com>
2017-08-11 13:28:20 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import stockstats
+from stockstats import StockDataFrame as Sdf
+from timaker import TiMaker
+import sys
+
+
+def main():
+    path = sys.argv[1].split("=")[1]
+    ti = TiMaker(path)
+    ti.run_all()
+    print(ti.final_data.head())
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+"path=data.csv".split("=")
---
+++ timaker.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import stockstats
+from stockstats import StockDataFrame as Sdf
+
+
+class TiMaker():
+    def __init__(self, path):
+        """
+        path: the path of the csv file containing stock data.
+        """
+        self.features = {
+        "Stocastic":['kdjd_5','kdjk_3','kdjk_3'],
+        "RSI":['rsi_14'],
+        "ADX":['adx_15_ema'],
+        "MACD":['macd', 'macds', 'macdh'],
+        "BBands":['boll', 'boll_ub', 'boll_lb'],
+        "ATR":['atr'],
+        }
+        self.path = path
+        self.stockdf = None
+        self.col_names = None
+        self.final_data = None
+
+    def read_df(self):
+        """Read csv stock data and convert it to stock data frame."""
+        self.stockdf = Sdf.retype(pd.read_csv(self.path))
+        self.col_names = list(self.stockdf.columns)
+
+    def feature_maker(self):
+        """Make features"""
+        #loop over the keys of the features dict
+        for ind in self.features.keys():
+            #loop over the commands in the value (list) for the ind key
+            for comm in self.features[ind]:
+                col_name = str(ind+'_'+comm)
+                #append the new features to the col_names list
+                self.col_names.append(col_name)
+                #creat  new feature as a column in the datafame
+                self.stockdf[col_name] = self.stockdf.get(comm)
+
+    def make_final_data(self):
+        """The final dataframe."""
+        self.final_data = self.stockdf[self.col_names].replace(10.000000,np.nan).dropna()
+
+    def run_all(self):
+        """Run all functions and return the final dataframe."""
+        self.read_df()
+        self.feature_maker()
+        self.make_final_data()
